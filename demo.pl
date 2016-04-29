1cfe37be59991f30ce0d42a566728417728d0f2d
Joe Johnston <jjohn@taskboy.com>
2016-04-29 16:07:34 -0400

    Model works, but simulation requires tuning

--- demo.pl
+++ demo.pl
@@ -1,107 +1,148 @@
 # Get the gist of the game using a few dumb bots
-
 use strict;
 use warnings;
-sub POE::Kernel::ASSERT_DEFAULT () { 1 };
-use POE;
-use POE::Component::Logger;
+use Time::HiRes;
 use Data::Dumper;
 
-my $heap = {
-            players => [],
-           };
 
-POE::Component::Logger->spawn(ConfigFile => "logger.cfg");
+package World;
 
-# Create three bots with different strategies
-my $alias = 'player1';
-POE::Session->create(heap => $heap, 
-                              inline_states => {
-                                                '_default' => sub {
-                                                    my ($event, $args) = @_[ARG0, ARG1];
-                                                    Logger->log(sprintf("[%d] default for event '$event'",
-                                                                        $_[SESSION]->ID
-                                                                       ));
-                                                },
-                                                'turn' => sub {
-# REALLY need three stages: PRODUCTION, SELLING, BUYING
-                                                    sleep 3;
-                                                    Logger->log(sprintf("[%d] ends its turn",
-                                                                        $_[SESSION]->ID
-                                                                       ) 
-                                                               );
-                                                },
-                                                _stop => sub { 
-                                                    Logger->log("STOPPING ID: " . $_[SESSION]->ID);
-                                                },
-                                                _start => sub {
-                                                    Logger->log(sprintf("%d session initialized", $_[SESSION]->ID));
-                                                    $_[KERNEL]->alias_set($alias);
-                                                }
-                                               });
+sub new {
+    my $class = shift;
+    my %self = @_;
+    
+    $self{caps}   = 100;
+    $self{energy} = 100;
+    $self{ore}    = 100;
+    bless \%self, $class;
+    \%self;
+}
 
-push @{$heap->{players}}, $alias;
+sub fn {
+    my ($self, $x) = @_;
+    $x *= 1.0;
+    return ($x**2.0) - (4.0 * $x) + 4.0
+}
 
-$alias = 'player2';
-POE::Session->create(heap => $heap, 
-                              inline_states => {
-                                                '_default' => sub {
-                                                    my ($event, $args) = @_[ARG0, ARG1];
-                                                    Logger->log(sprintf("[%d] default for event '$event'",
-                                                                        $_[SESSION]->ID
-                                                                       ));
-                                                },
-                                                'turn' => sub {
-                                                    sleep 1;
-                                                    Logger->log(sprintf("[%d] ends its turn",
-                                                                        $_[SESSION]->ID
-                                                                       ) 
-                                                               );
+sub unit_price {
+    my ($self, $product) = @_;
 
-                                                },
-                                                _stop => sub { 
-                                                    Logger->log("STOPPING ID: " . $_[SESSION]->ID);
-                                                },
-                                                _start => sub {
-                                                    Logger->log(sprintf("%d session initialized", $_[SESSION]->ID));
-                                                    $_[KERNEL]->alias_set($alias);
-                                                }
-                                               });
-push @{$heap->{players}}, $alias;
-# Create a dungeon master to update the world
-POE::Session->create(heap => $heap,
-                     inline_states => {
-                                       '_default' => sub {
-                                           my ($event, $args) = @_[ARG0, ARG1];
-                                           Logger->log(sprintf("[%d] default for event '$event'",
-                                                               $_[SESSION]->ID
-                                                              ));
-                                       },
-                                       
-                                       'run_players' => sub {
-                                           my ($heap) = ($_[HEAP]);
-                                           # Logger->log(Dumper(\@_));
+    my $supply = $self->{$product};
+    # Map the supply into the domain of the demand curve (200 units)
+    my $over_abundance = 200.0;
+    my $base_cost = 10.0;
+    
+    if ($supply > $over_abundance) {
+        print "World glut of $product: $supply units available\n";
+        $supply = $over_abundance;
+    }
 
-                                           for my $player_id (@{$heap->{players}}) {
-                                               Logger->log("KERNEL: turn for $player_id");
-                                               $_[KERNEL]->post($player_id, "turn");
-                                           }
-                                       },
-                                       
-                                       'maint' => sub {
-                                           Logger->log("KERNEL: Maint");
-                                           $_[KERNEL]->yield("run_players");
-                                           
-                                           $_[KERNEL]->delay("maint" => 1);
-                                           
-                                       },
-                                       _start => sub {
-                                           Logger->log("KERNEL: ID is " . $_[SESSION]->ID);
-                                           $_[KERNEL]->yield("maint");
-                                       }
-                                      });
+    if ($supply < 0) {
+        $supply = 0;
+    }
 
-print "Starting\n";
-POE::Kernel->run();
+    if ($supply < 1.0) {
+        printf "World shortage of $product: %0.2f units available\n", $supply;
+    }
+    
+    # Range of X values 0 - 2
+    my $scalar = $self->fn(2.0 * ($supply / $over_abundance));
+    return $scalar * $base_cost;
+}
 
-exit;
+package Actor;
+our $ID = 0;
+sub new {
+    my $class = shift;
+    my %self = @_;
+    $self{id} = $Actor::ID++;
+    $self{caps} = 0;
+    $self{energy} = 0;
+    $self{ore} = 0;
+    bless \%self, $class;
+    \%self;
+}
+
+sub sell {
+    my ($self, $product) = @_;
+
+    for my $product ('ore', 'energy') {
+        my $want = int(rand() * 30);
+        while ($want > $self->{$product}) {
+            $want = int(rand() * 30);
+        }
+        
+        next if $want < 1;
+        
+        $self->{$product} -= $want;
+        my $unit_price = $self->{world}->unit_price($product);
+        $self->{caps} += $unit_price * $want; # fiat money
+        printf $self->{id} . ": sells $want units of $product \@ %0.2f caps/unit\n", $unit_price;
+    }
+
+}
+
+sub buy {
+    my ($self, $product) = @_;
+    
+    for my $product ('ore', 'energy') {
+        my $want = int(rand() * 40);
+        while ($want > $self->{world}->{$product}) {
+            $want = int(rand() * 30);
+        }
+        
+        $self->{world}->{$product} -= $want;
+        my $unit_price = $self->{world}->unit_price($product);
+        if (($unit_price * $want) < $self->{caps}) {
+            printf $self->{id} . ": buys $want units of $product \@ %0.2f caps/unit\n", $unit_price;
+            $self->{caps} -= $unit_price * $want;
+        } else {
+           print($self->{id} . ": cannot buy $want units of $product. Not enough caps\n");
+        }
+    }
+}
+
+
+sub produce {
+    my ($self) = @_;
+
+    for my $product ('ore', 'energy') {
+        my $harvest = int(rand() * 10);
+        $self->{$product} += $harvest;
+        print $self->{id} . ": produced $harvest units of $product\n";
+    }
+    
+}
+
+
+package main;
+
+Main();
+
+sub Main {
+    my $world = World->new();
+    my @actors;
+    for (0..3) {
+        push @actors, Actor->new(world => $world);
+    }
+
+    my $week = 0;
+    while (1) {
+        printf "=====Week %03d====\n", $week++;
+        
+        for my $actor (@actors) {
+            $actor->produce;
+            $actor->sell;
+            print "----\n";
+        }
+        
+        for my $actor (@actors) {
+            $actor->buy;
+            print "----\n";
+        }
+        
+        sleep(3);
+    }
+}
+
+1;
